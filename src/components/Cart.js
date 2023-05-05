22b64a65a63ced31886c80f7bdfe2a00bc34f8ce
sujan pal <sujanpal79@gmail.com>
2023-05-05 12:43:46 +0530

    Updated Clear cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { useSelector } from 'react-redux' 
+import { useDispatch, useSelector } from 'react-redux' 
 import FoodItem from './FoodItem'
+import { clearCart } from '../utils/cartSlice';
 
 function Cart() {
   const cartItems = useSelector(store => store.cart.items )
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  }
   return (
     <div>
+      <button onClick={()=> handleClearCart()}> Clear Cart</button>
         <h1>Cart Items - { cartItems.length}</h1>
         
         {cartItems.map((item) =>(
